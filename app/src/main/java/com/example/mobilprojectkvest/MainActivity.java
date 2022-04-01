bb10ca62aec29750c03665331cd2bdce0317a719
Максим Майнагашев <maxuser99@mail.ru>
2022-04-02 00:19:24 +0700

    auth page added

--- app/src/main/java/com/example/mobilprojectkvest/MainActivity.java
+++ app/src/main/java/com/example/mobilprojectkvest/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.mobilprojectkvest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String login;
+    private String password;
+
+    private EditText loginTxt;
+    private EditText passwordTxt;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginTxt = findViewById(R.id.loginTxt);
+        passwordTxt = findViewById(R.id.passwordTxt);
+    }
+
+    private void OnLoginClick(View view)
+    {
+        login = loginTxt.getText().toString();
+        password = passwordTxt.getText().toString();
+
+        if (login == "" || password == "")
+        {
+            Toast toast = Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
