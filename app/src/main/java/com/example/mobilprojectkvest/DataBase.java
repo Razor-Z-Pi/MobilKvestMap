5cc76643ee53d5f190fc29ae51589c50c6c62434
Razor_Z_Pi <popovp.popoff2001@gmail.com>
2022-04-04 00:34:47 +0700

    AuthDB ver_0.1

---
+++ app/src/main/java/com/example/mobilprojectkvest/DataBase.java
@@ -0,0 +1,48 @@
+package com.example.mobilprojectkvest;
+
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+
+public class DataBase extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "kvestDB.db"; // Навзвание БД
+    private static final int SCHEMA = 3; // Версия БД
+    static final String TABLE_AUTH = "Authorization";
+    static final String TABLE_USER = "User";
+    static final String TABLE_KVEST = "Kvest";
+
+    public DataBase(Context context) {
+        super(context, DATABASE_NAME, null, SCHEMA);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //Создание таблицы авторизации
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_AUTH + " (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "login TEXT," +
+                "password TEXT);");
+        // Создание таблицы справочника польщователя
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_USER + " (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "nickname TEXT," +
+                "id_kvest INTEGER," +
+                "id_auth INTEGER," +
+                "FOREIGN KEY (id_kvest) REFERENCES "+ TABLE_KVEST +"(id)," +
+                "FOREIGN KEY (id_auth) REFERENCES "+ TABLE_AUTH +"(id));");
+        // Создание таблицы с квестами
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_KVEST + " (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "namePoint TEXT," +
+                "X REAL," +
+                "Y REAL);");
+
+        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_AUTH + " (login, password) VALUES ('admin', '123');" );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_AUTH);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_KVEST);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+        onCreate(sqLiteDatabase);
+    }
+}
--- app/src/main/java/com/example/mobilprojectkvest/MainActivity.java
+++ app/src/main/java/com/example/mobilprojectkvest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mobilprojectkvest;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
     private EditText loginTxt;
     private EditText passwordTxt;
     private Button loginButton;
+    private Button RegisterButton;
+
+    private DataBase databaseSource;
+    private SQLiteDatabase db;
+    private Cursor Auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,44 +34,79 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testLogin = "foo";
-        testPassword = "foo";
         loginTxt = findViewById(R.id.loginTxt);
         passwordTxt = findViewById(R.id.passwordTxt);
         loginButton = findViewById(R.id.loginBtn);
+        RegisterButton = findViewById(R.id.RegisterBTN);
 
-        View.OnClickListener loginClickListener = new View.OnClickListener() {
+        View.OnClickListener loginClickListener = new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                OnLoginClick();
+            public void onClick(View view)
+            {
+                // Open DB
+                db = databaseSource.getReadableDatabase();
+                OnLoginClick(db);
             }
         };
 
-        loginButton.setOnClickListener(loginClickListener);
+        View.OnClickListener RegisterClickListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                OnRegister();
+            }
 
+        };
+
+        loginButton.setOnClickListener(loginClickListener);
+        RegisterButton.setOnClickListener(RegisterClickListener);
+
+        databaseSource = new DataBase(getApplicationContext());
     }
 
-    private void OnLoginClick()
+    private void OnLoginClick(SQLiteDatabase db)
     {
         login = loginTxt.getText().toString();
         password = passwordTxt.getText().toString();
 
-        if (login.equals("") || password.equals(""))
-        {
-            Toast toast = Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT);
-            toast.show();
-        }
-        else
-        {
+        // Получаем данные
+        Auth = db.rawQuery("SELECT * FROM " + databaseSource.TABLE_AUTH + ";", null);
 
-            //надо чтоб данные из базы сравнивались :( Java
-            if (login.equals(testLogin) && password.equals(testPassword))
-            {
-                //Intent intent = new Intent(this, MenuPage.class);
-                //startActivity(intent);
-                Toast toast1 = Toast.makeText(this, "Auth succeded", Toast.LENGTH_SHORT);
-                toast1.show();
+        try {
+            if (login.equals("") || password.equals("")) {
+                Toast toast = Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT);
+                toast.show();
+            } else {
+                if (Auth.moveToFirst()) {
+                    for (int i = 0; i < Auth.getCount(); i++) {
+                        String loginAuth = Auth.getString(1);
+                        String passwordAuth = Auth.getString(2);
+                        if (login.equals(loginAuth) && password.equals(passwordAuth)) {
+                            Toast toast1 = Toast.makeText(this, "Auth succeded", Toast.LENGTH_SHORT);
+                            toast1.show();
+                            return;
+                        }
+                    }
+                    Toast Warning = Toast.makeText(this, "Такого пользователя нету", Toast.LENGTH_SHORT);
+                    Warning.show();
+                    Auth.close();
+                    db.close();
+                    return;
+                } else {
+                    Toast Warning = Toast.makeText(this, "Что-то не так", Toast.LENGTH_SHORT);
+                    Warning.show();
+                    return;
+                }
             }
+        } catch (Exception exception) {
+            Toast error = Toast.makeText(this, exception.toString(), Toast.LENGTH_SHORT);
+            error.show();
         }
+    }
+
+    private void OnRegister()
+    {
+
     }
 }
